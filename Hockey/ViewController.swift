b4e6436e9c6f12ff566620b3301171329bfbba9a
Laura West <laurawest@lauras-macbook-pro.local>
2018-04-26 14:35:07 -0600

    Initial Commit

---
+++ Hockey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hockey
+//
+//  Created by Laura West on 4/26/18.
+//  Copyright © 2018 Kyle Brittain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
